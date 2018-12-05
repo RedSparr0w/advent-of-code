5c4dc2d1b2802fc4d402a00db36f7eb0808bc243
Danial Nickford <admin@redsparr0w.com>
2018-12-05 16:20:45 +1300

    [Day 1 | Part 2] Optimize - array → set

--- 2018/Day_01/part_2.js
+++ 2018/Day_01/part_2.js
@@ -1,14 +1,14 @@
 const input = require('./input');
 
-var frequency_history = [];
+var frequency_history = new Set([0]);
 
 function get_first_duplicate(start_frequency, tries = 1){
   var frequency = start_frequency
   var found = input.some((change)=>{
-    if (frequency_history.includes(frequency += (+change))){
+    if (frequency_history.has(frequency += (+change))){
       return true;
     }
-    frequency_history.push(frequency);
+    frequency_history.add(frequency);
     return false;
   });
   console.log('Tries:', tries);
