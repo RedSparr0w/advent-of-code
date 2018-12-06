cd757152414ee32a76be20a22a5b77c91d9d5fa4
Danial Nickford <admin@redsparr0w.com>
2018-12-06 13:10:04 +1300

    [Day 5 | Part 1] Sort regex

--- 2018/Day_05/part_1.js
+++ 2018/Day_05/part_1.js
@@ -3,7 +3,7 @@ let input = require('./input');
 previousLength = 0;
 while (input.length != previousLength){
   previousLength = input.length;
-  input = input.replace(/(aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Zz|Yy|Xx|Ww|Vv|Uu|Tt|Ss|Rr|Qq|Pp|Oo|Nn|Mm|Ll|Kk|Jj|Ii|Hh|Gg|Ff|Ee|Dd|Cc|Bb|Aa)/g,'');
+  input = input.replace(/(Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz|aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ)/g,'');
 }
 
 console.log('Final length:', input.length);
