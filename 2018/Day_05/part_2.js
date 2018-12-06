ed9e6ab5edc295bcfb8bc90a9f52af2ef360bcb7
Danial Nickford <admin@redsparr0w.com>
2018-12-06 13:11:59 +1300

    [Day 5 | Part 2] Sort regex, Sort alphabet, lewestLength = input.length

--- 2018/Day_05/part_2.js
+++ 2018/Day_05/part_2.js
@@ -1,16 +1,16 @@
 let input = require('./input');
 
-let alphabet =  'qwertyuiopasdfghjklzxcvbnm'.split('');
-let lowestLength;
+let alphabet =  'abcdefghijklmnopqrstuvwxyz'.split('');
+let lowestLength = input.length;
 for(i = 0; i < alphabet.length; i++){
   let reg = new RegExp(`${alphabet[i]}`,'gi');
   let tempInput = input.replace(reg, '');
   let previousLength = 0;
   while (tempInput.length != previousLength){
     previousLength = tempInput.length;
-    tempInput = tempInput.replace(/(aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Zz|Yy|Xx|Ww|Vv|Uu|Tt|Ss|Rr|Qq|Pp|Oo|Nn|Mm|Ll|Kk|Jj|Ii|Hh|Gg|Ff|Ee|Dd|Cc|Bb|Aa)/g,'');
+    tempInput = tempInput.replace(/(Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz|aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ)/g,'');
   }
-  if (!lowestLength || tempInput.length < lowestLength)
+  if (tempInput.length < lowestLength)
     lowestLength = tempInput.length;
 }
 
