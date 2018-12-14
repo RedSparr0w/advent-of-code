3b4e7f7879bec6f4d6757379303c8b969076e045
RedSparr0w <admin@redsparr0w.com>
2018-12-14 20:38:20 +1300

    [Day 14 | Part 1] Shrink functions

--- 2018/Day_14/part_1.js
+++ 2018/Day_14/part_1.js
@@ -5,23 +5,12 @@ let e1 = 0,
     e2 = 1;
 const recipes = [3, 7];
 while (recipes.length < learning_recipes + 10){
-  let new_recipes = sumCurrentRecipes().split('').map(Number);
-  recipes.push(...new_recipes);
-  moveToNextRecipe();
+  let v1 = recipes[e1],
+      v2 = recipes[e2];
+  recipes.push(...(`${v1 + v2}`).split('').map(Number));
+  e1 = (e1 + recipes[e1] + 1) % recipes.length;
+  e2 = (e2 + recipes[e2] + 1) % recipes.length;
 }
 
 console.timeEnd('Execution Time');
 console.log(recipes.slice(learning_recipes).join(''));
-
-function sumCurrentRecipes(){
-  let sum = 0;
-  sum += recipes[e1];
-  sum += recipes[e2];
-  return `${sum}`;
-}
-
-function moveToNextRecipe(){
-  e1 = (e1 + recipes[e1] + 1) % recipes.length;
-  e2 = (e2 + recipes[e2] + 1) % recipes.length;
-  return;
-}
