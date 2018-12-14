06ec27b821c85911ee436b68f0452470e50c67b4
RedSparr0w <admin@redsparr0w.com>
2018-12-14 20:32:34 +1300

    [Day 14 | Part 1] Optimize

--- 2018/Day_14/part_1.js
+++ 2018/Day_14/part_1.js
@@ -1,7 +1,8 @@
 const learning_recipes = +require('./input');
 
 console.time('Execution Time');
-const elves = [{current: 0}, {current: 1}];
+let e1 = 0,
+    e2 = 1;
 const recipes = [3, 7];
 while (recipes.length < learning_recipes + 10){
   let new_recipes = sumCurrentRecipes().split('').map(Number);
@@ -14,15 +15,13 @@ console.log(recipes.slice(learning_recipes).join(''));
 
 function sumCurrentRecipes(){
   let sum = 0;
-  for(elf in elves){
-    sum += recipes[elves[elf].current];
-  }
+  sum += recipes[e1];
+  sum += recipes[e2];
   return `${sum}`;
 }
 
 function moveToNextRecipe(){
-  for(elf in elves){
-    elves[elf].current = (elves[elf].current + recipes[elves[elf].current] + 1) % recipes.length;
-  }
+  e1 = (e1 + recipes[e1] + 1) % recipes.length;
+  e2 = (e2 + recipes[e2] + 1) % recipes.length;
   return;
 }
